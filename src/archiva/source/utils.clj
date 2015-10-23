25117feeeceecda2997b13ff3d6bab6d6d2871b1
Greg Look <greg@greg-look.net>
2015-10-23 16:21:31 -0700

    Change coverage-gaps to be lazy-sequence based.

--- src/archiva/source/utils.clj
+++ src/archiva/source/utils.clj
@@ -17,36 +17,31 @@
   resulting intervals will not be longer than the `limit` period, if set. If
   the target is already completely covered, returns an empty list."
   [target coverage limit]
-  (loop [goals []
-         t (time/start target)
-         covers (sort-by time/start coverage)]
-    (if (time/before? t (time/end target))
-      (if-let [cover (first covers)]
-        ; Check next covering interval.
-        (if (time/before? t (time/start cover))
-          ; Need to cover up to the start of the next interval.
-          (let [goal (goal-interval t
-                       (time/earliest (time/start cover) (time/end target))
-                       limit)]
-            ; Did we make it to the next covered interval?
-            (if (time/abuts? goal cover)
-              ; Yes! Advance to the end of the next cover.
-              (recur (conj goals goal)
-                     (time/end cover)
-                     (rest covers))
-              ; Nope. Advance to the end of the goal, keep cover.
-              (recur (conj goals goal)
-                     (time/end goal)
-                     covers)))
-          ; Time is inside the covering interval. Advance to the end of the
-          ; cover if it's past the current time.
-          (recur goals
-                 (time/latest t (time/end cover))
-                 (rest covers)))
-        ; No covering intervals, try to reach for the end of the target.
-        (let [goal (goal-interval t (time/end target) limit)]
-          (recur (conj goals goal)
-                 (time/end goal)
-                 nil)))
-      ; Current time is past the end of target interval.
-      goals)))
+  (let [next-goal
+        (fn next-goal
+          [[_ t covers]]
+          (when (and t (time/before? t (time/end target)))
+            (if-let [cover (first covers)]
+              ; Check next covering interval.
+              (if (time/before? t (time/start cover))
+                ; Need to cover up to the start of the next interval.
+                (let [goal (goal-interval t
+                             (time/earliest (time/start cover) (time/end target))
+                             limit)]
+                  ; Did we make it to the next covered interval?
+                  (if (time/abuts? goal cover)
+                    ; Yes! Advance to the end of the next cover.
+                    [goal (time/end cover) (rest covers)]
+                    ; Nope. Advance to the end of the goal, keep cover.
+                    [goal (time/end goal) covers]))
+                ; Time is inside the covering interval. Advance to the end of the
+                ; cover if it's past the current time.
+                [nil (time/latest t (time/end cover)) (rest covers)])
+              ; No covering intervals, try to reach for the end of the target.
+              (let [goal (goal-interval t (time/end target) limit)]
+                [goal (time/end goal) nil]))))]
+    (->> [nil (time/start target) (sort-by time/start coverage)]
+         (iterate next-goal)
+         (take-while some?)
+         (map first)
+         (remove nil?))))
